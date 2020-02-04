fac4c084617804ec554a3ef0908cd84914fa6e51
Młyński Stanisław <mlynski.stanislaw@gmail.com>
2020-02-04 16:58:32 +0100

    Ready.

--- 2DArrays/Program.cs
+++ 2DArrays/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace _2DArrays
 {
@@ -10,6 +7,24 @@ namespace _2DArrays
     {
         static void Main(string[] args)
         {
+            string[,] array2d = new string[3, 5];
+
+            for (int i = 0; i < array2d.GetLength(0); i++)
+            {
+                for (int j = 0; j < array2d.GetLength(1); j++)
+                {
+                    array2d[i, j] = i + j.ToString();
+                }
+            }
+
+            for (int i = 0; i < array2d.GetLength(0); i++)
+            {
+                for (int j = 0; j < array2d.GetLength(1); j++)
+                {
+                    Console.WriteLine(array2d[i, j]);
+                }
+            }
+            Console.ReadKey();
         }
     }
 }
